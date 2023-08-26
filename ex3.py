ccdc733bb2de8a392be9c9f593308f2149368ee9
Danila <strong.pazenko@mail.ru>
2023-08-26 22:38:21 +0300

    ex3

---
+++ ex3.py
@@ -0,0 +1,15 @@
+print("Vrite four numbers: ")
+a = input()
+b = input()
+c = input()
+d = input()
+a = int(a)
+b = int(b)
+c = int(c)
+d = int(d)
+q = a + b
+w = c + d
+e = q / w
+e = float(e)
+print("Results: %.2f" % e)
+input('Press ENTER to exit')
